650ec15d3356ef524583f85ecd6fa24dd8132a26
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2023-06-27 13:13:26 +0900

    no message

--- front/login.js
+++ front/login.js
@@ -3,6 +3,10 @@ const form = document.querySelector("#login-form");
 const handleSubmit = async (event) => {
   event.preventDefault();
   const formData = new FormData(form);
+
+  //sha256은 단방향. 즉, 복호화가 불가능. 따라서 이렇게 암호화를 한 것 끼리 비교해야 한다.
+  //회원가입시 암호화하여 저장하고, 로그인시에도 암호화하여 암호문끼리 비교하는 것.
+  //같은 평문을 암호화하면 암호문도 같기 때문에, 비교가 가능하다.
   const sha256Password = sha256(formData.get("password"));
   formData.set("password", sha256Password);
 
